2631215e311481f7a6c1aa2af13d3368ff2ba3e8
Atul310 <atuldhital2310@gmail.com>
2023-01-06 15:10:45 +0545

    add

---
+++ edabit.py
@@ -0,0 +1,30 @@
+#edabit easya
+
+#curzon
+#reversing  using slicing 
+t = "atul"
+print(t[-1:-5:-1])
+
+
+
+##edabit easy 
+#problem 4
+def curzon (n):
+    a = 2**n+1
+    b = 2*n+1
+    if (a%b==0):
+     return True 
+    else :
+      return False
+  
+
+print(curzon(7))
+
+
+
+#problem5
+
+
+
+
+
